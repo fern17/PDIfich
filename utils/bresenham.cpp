56068c53314668a6ffd6a62f7e88e618f8228b04
marian0 <marianosantafe@gmail.com>
2013-03-19 09:26:41 -0300

    Se modificó bresennham a la forma sin optimizacion

--- utils/bresenham.cpp
+++ utils/bresenham.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <cassert>
 
 namespace utils{
 
@@ -11,62 +12,81 @@ void get_bresenham_coords(  unsigned int H,
 							unsigned int y1,
 							std::vector<std::vector<unsigned int> > & coords
 						)	{
-	coords.clear();
-	//delta_y > delta_x ?
-	bool step = std::abs(y1-y0) > std::abs(x1-x0);
-
-	unsigned int temp;
-	if (step) {
-		temp = y0;
-		y0 = x0;
-		x0 = temp;
-
-		temp = y1;
-		y1 = x1;
-		x1 = temp;
-	}
-
-	if (x0 > x1) {
-		temp = x0;
-		x0 = x1;
-		x1 = temp;
-
-		temp = y0;
-		y0 = y1;
-		y1 = temp;
-	}
-
 	int deltax = x1 - x0;
+	assert(deltax != 0);
 	int deltay = y1 - y0;
-
-	int error = deltax / 2;
-
-	int ystep;
-	int y=y0;
-
-	if (y0 < y1)
-		ystep = 1;
-	else
-		ystep = -1;
-    
-	for (unsigned int x=x0; x < x1; x++) {
+	float error = 0;
+	float deltaerr = fabs( ((float) deltay) / ((float) deltax)  );
+	int y = y0;
+	for (int x = x0; x < x1; x++) {
 		std::vector<unsigned int> coord;
-		if (step) {
-			coord.push_back(y);
-			coord.push_back(x);
-			coords.push_back(coord);
-		} else {
-			coord.push_back(x);
-			coord.push_back(y);
-			coords.push_back(coord);
-		}
-		error = error - deltay;
-		if (error < 0) {
-			y = y + ystep;
-			error = error + deltax;
-		}
+		coord.push_back(x);
+		coord.push_back(y);
+		coords.push_back(coord);
 
+		error = error + deltaerr;
+
+		if (error >= 0.5) {
+			y = y + 1;
+			error = error - 1.0;
+		}
 	}
+// 	coords.clear();
+// 	//delta_y > delta_x ?
+// 	bool step = std::abs(y1-y0) > std::abs(x1-x0);
+
+// 	unsigned int temp;
+// 	if (step) {
+// 		temp = y0;
+// 		y0 = x0;
+// 		x0 = temp;
+
+// 		temp = y1;
+// 		y1 = x1;
+// 		x1 = temp;
+// 	}
+
+// 	if (x0 > x1) {
+// 		temp = x0;
+// 		x0 = x1;
+// 		x1 = temp;
+
+// 		temp = y0;
+// 		y0 = y1;
+// 		y1 = temp;
+// 	}
+
+// 	int deltax = x1 - x0;
+// 	int deltay = y1 - y0;
+
+// 	int error = deltax / 2;
+
+// 	int ystep;
+// 	int y=y0;
+
+// 	if (y0 < y1)
+// 		ystep = 1;
+// 	else
+// 		ystep = -1;
+    
+// 	for (unsigned int x=x0; x < x1; x++) {
+// 		std::vector<unsigned int> coord;
+// 		if (step) {
+// 			coord.push_back(y);
+// 			coord.push_back(x);
+// 			coords.push_back(coord);
+// 		} else {
+// 			coord.push_back(x);
+// 			coord.push_back(y);
+// 			coords.push_back(coord);
+// 		}
+// 		error = error - deltay;
+// 		if (error < 0) {
+// 			y = y + ystep;
+// 			error = error + deltax;
+// 		}
+
+// 	}
 }
 
 } //End namespace
