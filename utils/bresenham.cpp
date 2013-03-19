d6ccac63df86614b6fd1a6ec8d40caaba252ac29
fern17 <fernando.nellmeldin@gmail.com>
2013-03-19 09:43:11 -0300

    Agrego otro bresenham

--- utils/bresenham.cpp
+++ utils/bresenham.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cmath>
 #include <cassert>
-
+#include <iostream>
 namespace utils{
 
 void get_bresenham_coords(  unsigned int H,
@@ -12,11 +12,31 @@ void get_bresenham_coords(  unsigned int H,
 							unsigned int y1,
 							std::vector<std::vector<unsigned int> > & coords
 						)	{
-	int deltax = x1 - x0;
+	coords.clear();
+    int dx = x1-x0,
+        dy = y1-y0,
+        y = y0,
+        eps = 0;
+    for (int x = x0; x <= x1; x++) {
+        std::vector<unsigned int> coord;
+        coord.push_back(x);
+        coord.push_back(y);
+        coords.push_back(coord);
+        eps += dy;
+        if ( (eps << 1) >= dx) {
+            y++;
+            eps -= dx;
+        }
+    }
+    
+    
+  /* 
+    int deltax = x1 - x0;
 	assert(deltax != 0);
 	int deltay = y1 - y0;
 	float error = 0;
 	float deltaerr = fabs( ((float) deltay) / ((float) deltax)  );
+    std::cout<<"Deltaerr = "<<deltaerr<<'\n';
 	int y = y0;
 	for (int x = x0; x < x1; x++) {
 		std::vector<unsigned int> coord;
@@ -27,10 +47,10 @@ void get_bresenham_coords(  unsigned int H,
 		error = error + deltaerr;
 
 		if (error >= 0.5) {
-			y = y + 1;
+			y = y - 1; //negativo porque las coordenadas estan al reves
 			error = error - 1.0;
 		}
-	}
+	}*/
 // 	coords.clear();
 // 	//delta_y > delta_x ?
 // 	bool step = std::abs(y1-y0) > std::abs(x1-x0);
