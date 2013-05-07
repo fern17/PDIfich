00a1e4aee827d8f67adef1240750011d8faeb40d
fern17 <fernando.nellmeldin@gmail.com>
2013-05-07 11:21:37 -0300

    Arregle el codigo del ejercicio 2

--- guia6-restauracion/ejercicio2.cpp
+++ guia6-restauracion/ejercicio2.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <ctime>
 using namespace cimg_library;   //Necesario
 
+const double EPSILON = 0.001;
 //Helper que te dice si un valor se va de rosca
 bool fueraDeRango(int x, int y, int w, int h) {
     if (y < 0 or y > h) 
@@ -16,61 +18,91 @@ bool fueraDeRango(int x, int y, int w, int h) {
 //Templatizado porque me gusta la reusabilidad
 template<typename T>
 //@ Devuelve el vecindario centrado en x0,y0 y de tamanio step_x*2 x step_y*2...
-std::vector<double> obtenerVecindario(CImg<T> &img, unsigned int x0, unsigned int y0, unsigned int w, unsigned int h, unsigned int step_x, unsigned int step_y) {
-    std::vector<double> ret_val; //Devuelve el vector de los pixeles del vecindario 
+//Los parametros se castean a int porque en los for hay resta que se autocastea al tipo de los operandos
+std::vector<double> obtenerVecindario(CImg<T> &img, int x0, int y0, unsigned int w, unsigned int h, int step_x, int step_y) {
 
-    for (int i = x0 - step_x; i <= x0 + step_x; i++) {        //Here be dragons... 
-        for (int j = y0 - step_y; j <= y0 + step_y; j++) {    //aca tambien hay dragones, rojos
-            if (not fueraDeRango(i, j, w, h))                   
+    std::vector<double> ret_val;                            
+
+    for (int i = x0 - step_x; i <= x0 + step_x; i++) {
+        for (int j = y0 - step_y; j <= y0 + step_y; j++) {
+            if (not fueraDeRango(i, j, w, h)) {            //Evita acceder a elementos fuera de la imagen
                 ret_val.push_back(img(i,j));
+            }
         }
     }
     return ret_val; 
 }
 
+//@ Calcula la media geometrica para un vecindario dado
 template<typename T>
 double mediaGeometrica(std::vector<T> vecindario) {
     double producto = 1.0;
+
+    //recorre el vecindario
     for (unsigned int ii = 0; ii < vecindario.size(); ii++) {
         producto *= vecindario[ii];
     }
-    producto = pow(producto,1.0/vecindario.size());
+
+    //Calcula raiz nm-esima
+    producto = std::pow(producto,1.0/vecindario.size());
     return producto;
 }
 
+//@ Calcula la media Contra Armonica de un vecindario, para un valor de Q dado. Contempla los casos especiales
 template<typename T>
 double mediaContraArmonica(std::vector<T> vecindario, double Q) {
+    //Inicializamos los contadores
     double suma_arriba = 0.0;
     double suma_abajo = 0.0;
-    for (unsigned int ii = 0; ii < vecindario.size(); ii++) {
-        //if (Q != -1)
-            suma_arriba += pow(vecindario[ii], Q+1);
-        //if (Q != 0)
-            suma_abajo  += pow(vecindario[ii], Q);
-    }
-    std::cout<<"arriba = "<<suma_arriba<<'\n';
-    std::cout<<"abajo = "<<suma_abajo<<'\n';
-    suma_arriba /= (float) vecindario.size();
-    suma_abajo  /= (float) vecindario.size();
 
-    double resultado = suma_arriba;
-    if (fabs(suma_abajo) > 0.01)
+    //Recorremos el vecindario
+    for (unsigned int ii = 0; ii < vecindario.size(); ii++) {
+        //Si el valor actual no es muy chico, exponencio, si no, es lo mismo que no sumar nada
+        if (std::fabs(vecindario[ii]) > EPSILON) {
+            if (Q != -1) //para cuando NO es media armonica
+                suma_arriba += std::pow(vecindario[ii], Q+1);
+            if (Q != 0)  //para cuando NO es media aritmetica
+                suma_abajo  += std::pow(vecindario[ii], Q);
+        }
+    }
+    
+    //Normalizamos si el vecindario tenia algun valor
+    if (vecindario.size() > 0) {
+        suma_arriba /= (float) vecindario.size();
+        suma_abajo  /= (float) vecindario.size();
+    }
+    
+    double resultado;
+    //Fix para media armonica, donde suma_arriba se hace 0
+    if (Q == -1)
+        resultado = 1.0;
+    else
+        resultado = suma_arriba;
+    
+    //Dividimos si suma_abajo tenia algun valor
+    if (fabs(suma_abajo) > EPSILON)
         resultado /= suma_abajo;
-    std::cout<<"resultado = "<<resultado<<'\n';
+
     return resultado;
 }
 
+//@ Calcula la media armonica llamando a mediaContraArmonica con el parametro Q necesario
 template<typename T>
 double mediaArmonica(std::vector<T> vecindario) {
     return mediaContraArmonica(vecindario, -1);
 }
 
+//@ Calcula la media aritmetica llamando a mediaContraArmonica con el parametro Q necesario
 template<typename T>
 double mediaAritmetica(std::vector<T> vecindario) {
     return mediaContraArmonica(vecindario, 0);
 }
 
-//@ Aplica la media geometrica 
+//@ Aplica el filtro de medias, para un Q determinado. Llama a los otros filtros de medias segun el parametro tipo:
+//tipo = 0: Media aritmetica
+//tipo = 1: Media geometrica
+//tipo = 2: Media armonica
+//tipo = 3: media contra armonica con el parametro Q necesario
 template<typename T>
 CImg<T> filtradoMedias(CImg<T> img, unsigned int tipo = 0, unsigned int ancho = 3, unsigned int alto = 3, double Q = 1.0) {
 
@@ -82,7 +114,6 @@ CImg<T> filtradoMedias(CImg<T> img, unsigned int tipo = 0, unsigned int ancho = 
     
     for (unsigned int i = 0; i < img.width(); i++) {
         for (unsigned int j = 0; j < img.height(); j++) {
-            std::cout<<"iteracion "<<i<<" "<<j<<"\n";
             std::vector<T> vecindario = obtenerVecindario(img, i, j, w, h, step_x, step_y);
             if (tipo == 0)
                 ret_val(i,j) = mediaAritmetica(vecindario);
@@ -94,39 +125,81 @@ CImg<T> filtradoMedias(CImg<T> img, unsigned int tipo = 0, unsigned int ancho = 
                 ret_val(i,j) = mediaContraArmonica(vecindario, Q);
         }
     }
+    
     return ret_val;
 }
+
+//@ Cambia el canal de intesidad de una imagen RGB, y la devuelve en RGB
+template <typename T>
+CImg<T> cambiarIntesidad(CImg<T> imagen, CImg<T> intesidad) {
+    if (imagen.spectrum() == 1) 
+        return imagen;
+    
+    //Convertimos a HSI
+    CImg<T> img = imagen.get_normalize(0,1).get_RGBtoHSI();
+    
+    //Separamos los 2 canales que nos interesan
+    CImg<T> canal1 = img.get_channel(0);
+    CImg<T> canal2 = img.get_channel(1);
+
+    //Componemos
+    CImg<T> ret_val;
+    ret_val = canal1;
+    ret_val.append(canal2, 'c');
+    intesidad.normalize(0,1);
+    ret_val.append(intesidad, 'c');
+
+    return ret_val.HSItoRGB().normalize(0,255);
+}
+
+
 int main(int argc, char *argv[]) {
+    //@ Calcula los filtros de medias para una imagen
 	const char* input = cimg_option("-i", "../images/sangre.jpg", "Input Image File");
     const double desvio_impulsivo = cimg_option("-s", 20, "Desvio sal y pimienta");
     const double desvio_gaussiano = cimg_option("-g", 20, "Desvio Gaussiano");
-    const double Q = cimg_option("-q", 1.0, "Parametro media contra armonica");
+    const double Q = cimg_option("-q", 1.2, "Parametro media contra armonica");
 	const unsigned int ancho = cimg_option("-w", 3, "Input Image File");
 	const unsigned int alto = cimg_option("-h", 3, "Input Image File");
 
     //Cargamos la imagen
     CImg<double> img (input);
-    img.crop(0,0,8,4);
+    CImg<double> img_original = img;
+
+    //Nos quedamos con su canal de intesidad
     img = img.RGBtoHSI().channel(2);
-    //Aplicamos ruido
-    CImg<double> img_ruidosa = img.get_noise(desvio_impulsivo, 2);
+    img.normalize(0,255);
+    
+    //Aplicamos ruido gaussiano e impulsivo
+    CImg<double> img_ruidosa = img;
+    img_ruidosa.noise(desvio_impulsivo, 2);
     img_ruidosa.noise(desvio_gaussiano, 0);
+    img_ruidosa.normalize(0,255);
+    img_ruidosa.save("img_ruidosa.jpg");
     
     //Aplicamos los filtros
-    std::cerr<<"wololo1\n";
+    std::cout<<"Aplicando media geometrica\n";
     CImg<double> resultado_geometrica = filtradoMedias(img_ruidosa, 1, ancho, alto).normalize(0,255);
     resultado_geometrica.save("resultado_geometrica.jpg");
-    std::cerr<<"wololo2\n";
+    
+    std::cout<<"Aplicando media Contra armonica\n";
     CImg<double> resultado_contraarmonica = filtradoMedias(img_ruidosa, 3, ancho, alto, Q).normalize(0,255);
     resultado_contraarmonica.save("resultado_contraarmonica.jpg");
-    std::cerr<<"wololo3\n";
+
+    std::cout<<"Aplicando media armonica\n";
     CImg<double> resultado_armonica = filtradoMedias(img_ruidosa, 2, ancho, alto).normalize(0,255);
     resultado_armonica.save("resultado_armonica.jpg");
-    std::cerr<<"wololo4\n";
+  
+    std::cout<<"Aplicando media aritmetica\n";
     CImg<double> resultado_aritmetica = filtradoMedias(img_ruidosa, 0, ancho, alto).normalize(0,255);
     resultado_aritmetica.save("resultado_artimetica.jpg");
-    std::cerr<<"wololo5\n";
 
-    (img, img_ruidosa, resultado_geometrica, resultado_contraarmonica, resultado_armonica, resultado_aritmetica).display();
+    //Computamos los resultados en color
+    CImg<double> r_geometrica = cambiarIntesidad(img_original, resultado_geometrica);
+    CImg<double> r_contraarmonica = cambiarIntesidad(img_original, resultado_contraarmonica);
+    CImg<double> r_armonica = cambiarIntesidad(img_original, resultado_armonica);
+    CImg<double> r_aritmetica = cambiarIntesidad(img_original, resultado_aritmetica);
+
+    (img_original, img_ruidosa, r_geometrica, r_contraarmonica, r_armonica, r_aritmetica).display();
     return 0;
 }
