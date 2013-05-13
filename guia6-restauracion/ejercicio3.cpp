525ca271ebf066691ed2c32359f3f30d25da4e72
fern17 <fernando.nellmeldin@gmail.com>
2013-05-13 11:57:15 -0300

    Mitad del 4 porque nos aburrimnos

---
+++ guia6-restauracion/ejercicio3.cpp
@@ -0,0 +1,164 @@
+#include <CImg.h>               //include basico de CImg
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+using namespace cimg_library;   //Necesario
+
+const double EPSILON = 0.001;
+//Helper que te dice si un valor se va de rosca
+bool fueraDeRango(int x, int y, int w, int h) {
+    if (y < 0 or y > h) 
+        return true;
+    if (x < 0 or x > w)
+        return true;
+    return false;
+}
+
+//Templatizado porque me gusta la reusabilidad
+template<typename T>
+//@ Devuelve el vecindario centrado en x0,y0 y de tamanio step_x*2 x step_y*2...
+//Los parametros se castean a int porque en los for hay resta que se autocastea al tipo de los operandos
+std::vector<double> obtenerVecindario(CImg<T> &img, int x0, int y0, unsigned int w, unsigned int h, int step_x, int step_y) {
+
+    std::vector<double> ret_val;                            
+
+    for (int i = x0 - step_x; i <= x0 + step_x; i++) {
+        for (int j = y0 - step_y; j <= y0 + step_y; j++) {
+            if (not fueraDeRango(i, j, w, h)) {            //Evita acceder a elementos fuera de la imagen
+                ret_val.push_back(img(i,j));
+            }
+        }
+    }
+    return ret_val; 
+}
+
+
+//@ Cambia el canal de intesidad de una imagen RGB, y la devuelve en RGB
+template <typename T>
+CImg<T> cambiarIntesidad(CImg<T> imagen, CImg<T> intesidad) {
+    if (imagen.spectrum() == 1) 
+        return imagen;
+    
+    //Convertimos a HSI
+    CImg<T> img = imagen.get_normalize(0,1).get_RGBtoHSI();
+    
+    //Separamos los 2 canales que nos interesan
+    CImg<T> canal1 = img.get_channel(0);
+    CImg<T> canal2 = img.get_channel(1);
+
+    //Componemos
+    CImg<T> ret_val;
+    ret_val = canal1;
+    ret_val.append(canal2, 'c');
+    intesidad.normalize(0,1);
+    ret_val.append(intesidad, 'c');
+
+    return ret_val.HSItoRGB().normalize(0,255);
+}
+
+//@ Devuelve la mediana de un vector calculando floor
+double mediana(std::vector<double> V) {
+    std::sort(V.begin(),V.end());
+    return V[std::floor(V.size()/2)];
+}
+
+double puntoMedio(std::vector<double> V) {
+    double maxi = *(std::max(V.begin(), V.end()));
+    double mini = *(std::min(V.begin(), V.end()));
+    return 0.5*(maxi + mini);
+}
+
+double alfaRecortado(std::vector<double> V, unsigned int alfa) {
+    std::sort(V.begin(), V.end());
+    std::vector<double> copia;
+    for (unsigned int i = std::floor(alfa/2); i < V.size() - std::floor(alfa/2); i++) {
+        copia.push_back(V[i]);
+    }
+    double ret_val = copia[std::floor(copia.size()/2)] / double(V.size() - alfa);
+    return ret_val;
+}
+
+//@ Calcula todos los filtros de orden
+//Tipo 0 = Mediana
+//Tipo 1 = Punto medio
+//Tipo 2 = Alfa Recortado
+template<typename T>
+CImg<T> filtradoOrden(CImg<T> img, unsigned int tipo = 0, unsigned int ancho = 3, unsigned int alto = 3, unsigned int alfa = 4) {
+
+    CImg<T> ret_val = img;
+    unsigned int h = img.height(); 
+    unsigned int w = img.width();  
+    unsigned int step_x = ancho/2;              //ancho del vecindario centrado
+    unsigned int step_y = alto/2;               //alto del vecindario centrado
+    
+    for (unsigned int i = 0; i < img.width(); i++) {
+        for (unsigned int j = 0; j < img.height(); j++) {
+            std::vector<T> vecindario = obtenerVecindario(img, i, j, w, h, step_x, step_y);
+            if (tipo == 0)
+                ret_val(i,j) = mediana(vecindario);
+            if (tipo == 1)
+                ret_val(i,j) = puntoMedio(vecindario);
+            if (tipo == 2)
+                ret_val(i,j) = alfaRecortado(vecindario, alfa);
+        }
+    }
+    
+    return ret_val;
+}
+
+int main(int argc, char *argv[]) {
+    //@ Calcula los filtros de medias para una imagen
+	const char* input = cimg_option("-i", "../images/sangre.jpg", "Input Image File");
+    const double desvio_impulsivo = cimg_option("-s", 20, "Desvio sal y pimienta");
+    const double desvio_gaussiano = cimg_option("-g", 20, "Desvio Gaussiano");
+	const unsigned int ancho = cimg_option("-w", 3, "Input Image File");
+	const unsigned int alto = cimg_option("-a", 3, "Input Image File");
+	const unsigned int alfa = cimg_option("-f", 2, "Input Image File");
+
+    //Cargamos la imagen
+    CImg<double> img (input);
+    CImg<double> img_original = img;
+
+    //Nos quedamos con su canal de intesidad
+    img = img.RGBtoHSI().channel(2);
+    img.normalize(0,255);
+    
+    //Aplicamos ruido gaussiano e impulsivo
+    CImg<double> img_ruidosa = img;
+    img_ruidosa.noise(desvio_impulsivo, 2);
+    img_ruidosa.noise(desvio_gaussiano, 0);
+    img_ruidosa.normalize(0,255);
+    img_ruidosa.save("img_ruidosa.jpg");
+    
+    //Aplicamos los filtros
+    std::cout<<"Aplicando mediana\n";
+    CImg<double> resultado_mediana = filtradoOrden(img_ruidosa, 0, ancho, alto).normalize(0,255);
+
+    std::cout<<"Aplicando punto medio\n";
+    CImg<double> resultado_puntomedio = filtradoOrden(img_ruidosa, 1, ancho, alto).normalize(0,255);
+
+    std::cout<<"Aplicando alfarecortado\n";
+    CImg<double> resultado_alfarecortado = filtradoOrden(img_ruidosa, 2, ancho, alto, alfa).normalize(0,255);
+
+    std::cout<<"Aplicando mediana y punto medio\n";
+    CImg<double> resultado_mpm = filtradoOrden(resultado_mediana, 1, ancho, alto).normalize(0,255);
+
+    CImg<double> r_mediana = cambiarIntesidad(img_original, resultado_mediana);
+    CImg<double> r_puntomedio = cambiarIntesidad(img_original, resultado_puntomedio);
+    CImg<double> r_alfarecortado = cambiarIntesidad(img_original, resultado_alfarecortado);
+    CImg<double> r_mpm = cambiarIntesidad(img_original, resultado_mpm); 
+
+    std::cout<<"\nErrores:\n";
+    std::cout<<"\nImagen ruidosa = "<<img_ruidosa.MSE(img);
+    std::cout<<"\nMediana = "<<resultado_mediana.MSE(img);
+    std::cout<<"\nPunto Medio = "<<resultado_puntomedio.MSE(img);
+    std::cout<<"\nAlfa Recortado = "<<resultado_alfarecortado.MSE(img);
+    std::cout<<"\nResultado Mediana y Punto Medio = "<<resultado_mpm.MSE(img);
+    std::cerr<<"\n";
+
+    (img_original, img_ruidosa, r_mediana, r_puntomedio, r_alfarecortado, r_mpm).display();
+
+    return 0;
+}
---
+++ guia6-restauracion/ejercicio4.cpp
@@ -0,0 +1,112 @@
+#include <CImg.h>               //include basico de CImg
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <utility>
+#include "../utils/fourier.cpp"
+#include "../utils/PDI_functions.h"
+using namespace cimg_library;   //Necesario
+
+bool dentroCirculo(int x, int y, int x0, int y0, unsigned int radio) {
+    return std::pow(x - x0, 2) + std::pow(y - y0, 2) - radio*radio < 0;
+}
+
+CImg<double> mascaraNotch(CImg<double> &img, std::vector<std::pair<unsigned int, unsigned int> > centros, std::vector<unsigned int> radios) {
+    CImg<double> ret_val(img.width(), img.height(), 1,1,1);
+    cimg_forXY(img,x,y) {
+        for (unsigned int i = 0; i < centros.size(); i++) {
+            if (dentroCirculo(int(x),int(y), int(centros[i].first), int(centros[i].second), radios[i])) {
+                ret_val(x,y) = 0;
+            }
+        }
+    }
+    return ret_val;
+}
+
+std::vector<std::pair<unsigned int, unsigned int> > leerCentros(std::string file) {
+    ifstream f (file.c_str());
+    if (!f.is_open()) {
+        std::cout<<"No pudo abrir el archivo "<<file<<"\n";
+        exit(-1);
+    }
+    unsigned int cantidad;
+    f>>cantidad;
+    std::vector<std::pair<unsigned int, unsigned int> > ret_val;
+    for (unsigned int i = 0; i < cantidad; i++) {
+        unsigned int x;
+        unsigned int y;
+        f>>x>>y;
+        ret_val.push_back(std::make_pair<unsigned int, unsigned int>(x,y));
+    }
+    f.close();
+    return ret_val;
+}
+
+std::vector<unsigned int> leerRadios(std::string file) {
+    ifstream f (file.c_str());
+    if (!f.is_open()) {
+        std::cout<<"No pudo abrir el archivo "<<file<<"\n";
+        exit(-1);
+    }
+    unsigned int cantidad;
+    f>>cantidad;
+    std::vector<unsigned int> ret_val;
+    for (unsigned int i = 0; i < cantidad; i++) {
+        unsigned int r;
+        f>>r;
+        ret_val.push_back(r);
+    }
+    f.close();
+    return ret_val;
+}
+
+int main(int argc, char *argv[]) {
+    //@ 
+	const char* input = cimg_option("-i", "../images/img_degradada.tif", "Input Image File");
+	const char* input2 = cimg_option("-i", "../images/img.tif", "Input Image File");
+	const char* f_centros = cimg_option("-c", "centros_ej4_1.txt", "Input Image File");
+	const char* f_radios = cimg_option("-r", "radios_ej4_1.txt", "Input Image File");
+
+    CImg<double> img(input);
+    CImg<double> img_original(input2);
+    unsigned int w = img.width();
+    unsigned int h = img.height();
+    CImgList<double> f_img = img.get_FFT();
+    
+    f_img[0].shift( w/2, h/2, 0, 0, 2);
+    f_img[1].shift( w/2, h/2, 0, 0, 2);
+    
+    CImg<double> magn = magn_tdf(img);
+
+    std::vector<unsigned int> radios = leerRadios(f_radios);
+    std::vector<std::pair<unsigned int, unsigned int> > centros = leerCentros(f_centros);
+
+    CImg<double> mascara_notch = mascaraNotch(img, centros, radios);
+    //mascara_notch.shift(w/2, h/2, 0, 0, 2);
+
+    CImgList<double> f_resultado;
+    CImg<double> r_r(w,h,1,1,0);
+    CImg<double> r_i(w,h,1,1,0);
+    
+    cimg_forXY(f_img[0],x,y) {
+        r_r(x,y) = f_img[0](x,y) * mascara_notch(x,y);
+        r_i(x,y) = f_img[1](x,y) * mascara_notch(x,y);
+    }
+
+    r_r.normalize(0,255);
+    r_i.normalize(0,255);
+    
+    f_resultado.push_back(r_r);
+    f_resultado.push_back(r_i);
+    
+    f_resultado[0].shift(w/2, h/2, 0, 0, 2);
+    f_resultado[1].shift(w/2, h/2, 0, 0, 2);
+    
+    CImg<double> resultado = f_resultado.get_FFT(true)[0];
+    resultado.normalize(0, 255);
+    resultado(0,0) = 0.0; //fix para display
+
+    std::cerr<<"Error cuadratico medio = "<<resultado.MSE(img_original)<<"\n";
+    (img, magn, mascara_notch, resultado, img_original).display();
+    return 0;
+}
