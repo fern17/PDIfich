a907e1c4e18b4c1622e5fbae15becc658e0c10b7
fern17 <fernando.nellmeldin@gmail.com>
2013-05-14 09:05:02 -0300

    Pull to mate

--- guia6-restauracion/ejercicio4.cpp
+++ guia6-restauracion/ejercicio4.cpp
@@ -11,18 +11,20 @@ bool dentroCirculo(int x, int y, int x0, int y0, unsigned int radio) {
     return std::pow(x - x0, 2) + std::pow(y - y0, 2) - radio*radio < 0;
 }
 
+//@ Realiza unna mascara notch para un vector de centros y radios
 CImg<double> mascaraNotch(CImg<double> &img, std::vector<std::pair<unsigned int, unsigned int> > centros, std::vector<unsigned int> radios) {
-    CImg<double> ret_val(img.width(), img.height(), 1,1,1);
+    CImg<double> ret_val(img.width(), img.height(), 1, 1, true);
     cimg_forXY(img,x,y) {
         for (unsigned int i = 0; i < centros.size(); i++) {
             if (dentroCirculo(int(x),int(y), int(centros[i].first), int(centros[i].second), radios[i])) {
-                ret_val(x,y) = 0;
+                ret_val(x,y) = false;
             }
         }
     }
     return ret_val;
 }
 
+//@ Lee los centros de circulos desde un archivo
 std::vector<std::pair<unsigned int, unsigned int> > leerCentros(std::string file) {
     ifstream f (file.c_str());
     if (!f.is_open()) {
@@ -42,6 +44,7 @@ std::vector<std::pair<unsigned int, unsigned int> > leerCentros(std::string file
     return ret_val;
 }
 
+//@ Lee los radios de los circulos desde un archivo
 std::vector<unsigned int> leerRadios(std::string file) {
     ifstream f (file.c_str());
     if (!f.is_open()) {
@@ -61,7 +64,7 @@ std::vector<unsigned int> leerRadios(std::string file) {
 }
 
 int main(int argc, char *argv[]) {
-    //@ 
+    //@ Aplica filtro notch pero no anda 
 	const char* input = cimg_option("-i", "../images/img_degradada.tif", "Input Image File");
 	const char* input2 = cimg_option("-i", "../images/img.tif", "Input Image File");
 	const char* f_centros = cimg_option("-c", "centros_ej4_1.txt", "Input Image File");
@@ -72,48 +75,33 @@ int main(int argc, char *argv[]) {
     unsigned int w = img.width();
     unsigned int h = img.height();
     CImgList<double> f_img = img.get_FFT();
-    
-    //f_img[0].shift( w/2, h/2, 0, 0, 2);
-    //f_img[1].shift( w/2, h/2, 0, 0, 2);
+
+    f_img[0].normalize(0,255);
+    f_img[1].normalize(0,255);
     
     CImg<double> magn = magn_tdf(img);
     CImg<double> magn2 = utils::get_magnitud(f_img, false).get_log().get_normalize(0,255);
-    magn2(w/2,h/2) = 255;
 
     std::vector<unsigned int> radios = leerRadios(f_radios);
     std::vector<std::pair<unsigned int, unsigned int> > centros = leerCentros(f_centros);
 
     CImg<double> mascara_notch = mascaraNotch(img, centros, radios);
     mascara_notch.shift(w/2, h/2, 0, 0, 2);
-
-    CImgList<double> f_resultado;
-    CImg<double> r_r(w,h,1,1,0);
-    CImg<double> r_i(w,h,1,1,0);
+  
+    CImgList<double> f_resultado(2,w,h,1,1,0.0);
     
     cimg_forXY(f_img[0],x,y) {
-        r_r(x,y) = f_img[0](x,y) * mascara_notch(x,y);
-        r_i(x,y) = 0;
-        //r_i(x,y) = f_img[1](x,y) * mascara_notch(x,y);
+        if (mascara_notch(x,y))
+            f_resultado[0](x,y) = f_img[0](x,y);
     }
 
-    r_r.normalize(0,255);
-    r_r(0,0) = 255;
-    //r_i.normalize(0,255);
-    
-    f_resultado.push_back(r_r);
-    f_resultado.push_back(r_i);
-    
-    //f_resultado[0].shift(w/2, h/2, 0, 0, 2);
-    //f_resultado[1].shift(w/2, h/2, 0, 0, 2);
-    
     CImg<double> resultado = f_resultado.get_FFT(true)[0];
-    resultado.normalize(0, 255);
-    resultado(0,0) = 0.0; //fix para display
+    //resultado(0,0) = 0.0; //fix para display
     
-    CImg<double> magn3 = utils::get_magnitud(f_resultado, false).get_normalize(0,255);
-    magn3(w/2,h/2) = 255;
-
+    //CImg<double> magn3 = utils::get_magnitud(f_resultado, false).get_normalize(0,255);
+    //magn3(w/2,h/2) = 0.0;
+    
     std::cerr<<"Error cuadratico medio = "<<resultado.MSE(img_original)<<"\n";
-    (img, magn2, mascara_notch, resultado, r_r.get_log(), r_i, magn3, img_original).display();
+    (img, mascara_notch, resultado, img_original).display();
     return 0;
 }
